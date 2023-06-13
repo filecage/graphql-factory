6d65e2d605d9c81af264e6c51efc91f63b71840d
David <filecage@users.noreply.github.com>
2023-06-13 11:33:51 +0200

    adds exceptions

---
+++ src/Exceptions/GraphQLFactoryException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Filecage\GraphQLFactory\Exceptions;
+
+class GraphQLFactoryException extends \Exception {}
---
+++ src/Exceptions/InvalidTypeException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Filecage\GraphQLFactory\Exceptions;
+
+class InvalidTypeException extends GraphQLFactoryException {}
