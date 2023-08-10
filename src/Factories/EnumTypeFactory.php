405316e21ba1513b09ba23c29b576df45d127c71
David <filecage@users.noreply.github.com>
2023-08-10 13:57:48 +0200

    use webonyx' internal enum implementation

--- src/Factories/EnumTypeFactory.php
+++ src/Factories/EnumTypeFactory.php
@@ -3,27 +3,15 @@
 namespace Filecage\GraphQL\Factory\Factories;
 
 use GraphQL\Type\Definition\EnumType;
+use GraphQL\Type\Definition\PhpEnumType;
 
 final class EnumTypeFactory implements TypeFactory {
 
     function __construct (private readonly \ReflectionEnum $reflectionEnum) {}
 
     function create (): EnumType {
-        return new EnumType([
-            'name' => $this->reflectionEnum->getShortName(),
-            'values' => [...$this->generateValues()]
-        ]);
+        // TODO: This is a double reflection (because `PhpEnumType` creates a new reflection of the enum internally)
+        return new PhpEnumType($this->reflectionEnum->name);
     }
 
-    private function generateValues () : \Generator {
-        foreach ($this->reflectionEnum->getCases() as $case) {
-            $value = ['name' => $case->name];
-
-            if ($case instanceof \ReflectionEnumBackedCase) {
-                $value['value'] = $case->getBackingValue();
-            }
-
-            yield $case->name => $value;
-        }
-    }
 }
