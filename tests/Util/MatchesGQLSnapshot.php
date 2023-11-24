a5d6595ef9d715d11a9ec95fef7a94dd20227c52
David <filecage@users.noreply.github.com>
2023-11-24 19:49:50 +0100

    adds additional check for execution result errors

--- tests/Util/MatchesGQLSnapshot.php
+++ tests/Util/MatchesGQLSnapshot.php
@@ -2,7 +2,9 @@
 
 namespace Filecage\GraphQL\FactoryTests\Util;
 
+use GraphQL\Executor\ExecutionResult;
 use GraphQL\Type\Schema;
+use Spatie\Snapshots\Drivers\JsonDriver;
 use Spatie\Snapshots\MatchesSnapshots;
 
 trait MatchesGQLSnapshot {
@@ -10,6 +12,14 @@ trait MatchesGQLSnapshot {
 
     function assertMatchesGraphQLSchemaSnapshot (Schema $schema, bool $assertValid = true) : void {
         $this->assertMatchesSnapshot($schema, new GraphQLDriver($assertValid));
+    }
+
+    function assertMatchesQuerySnapshot (ExecutionResult $executionResult) : void {
+        if (!empty($executionResult->errors)) {
+            throw new $executionResult->errors[0];
+        }
+
+        $this->assertMatchesSnapshot($executionResult, new JsonDriver());
     }
 
 }
