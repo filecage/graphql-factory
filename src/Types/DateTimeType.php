12478c22ff7409c2e008d07e1a05aa3e1d73cdfe
David <filecage@users.noreply.github.com>
2023-11-24 22:28:37 +0100

    DateTime fields are nonNull

--- src/Types/DateTimeType.php
+++ src/Types/DateTimeType.php
@@ -14,22 +14,22 @@ final class DateTimeType extends ObjectType implements Cacheable {
             'description' => 'DateTime in ISO8601 representation (YYYY-MM-DDTHH:MM:SSZ)',
             'fields' => [
                 'iso8601' => [
-                    'type' => Type::string(),
+                    'type' => Type::nonNull(Type::string()),
                     'description' => 'DateTime as ISO8601 string',
                     'resolve' => fn(DateTimeInterface $dateTime) => $dateTime->format('c'),
                 ],
                 'ymd' => [
-                    'type' => Type::string(),
+                    'type' => Type::nonNull(Type::string()),
                     'description' => 'Date only in the format of YYYY-MM-DD (e.g. 2023-08-22), all leading zeros',
                     'resolve' => fn(DateTimeInterface $dateTime) => $dateTime->format('Y-m-d'),
                 ],
                 'hms' => [
-                    'type' => Type::string(),
+                    'type' => Type::nonNull(Type::string()),
                     'description' => 'Time only in the format of H:M:S (e.g. 21:42:04)',
                     'resolve' => fn(DateTimeInterface $dateTime) => $dateTime->format('H:i:s'),
                 ],
                 'tz' => [
-                    'type' => Type::string(),
+                    'type' => Type::nonNull(Type::string()),
                     'description' => 'Timezone only as identifier representation',
                     'resolve' => fn(DateTimeInterface $dateTime) => $dateTime->format('e')
                 ]
